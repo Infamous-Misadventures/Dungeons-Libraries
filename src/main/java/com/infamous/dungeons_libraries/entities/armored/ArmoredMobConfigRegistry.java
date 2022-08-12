2740a42003bac2e52c950c1e86a18b3655b4b98f
Patrigan <patrick.provinciael@gmail.com>
2022-08-12 11:51:10 +0200

    Add list in between

--- src/main/java/com/infamous/dungeons_libraries/entities/armored/ArmoredMobConfigRegistry.java
+++ src/main/java/com/infamous/dungeons_libraries/entities/armored/ArmoredMobConfigRegistry.java
@@ -2,7 +2,6 @@ package com.infamous.dungeons_libraries.entities.armored;
 
 import com.infamous.dungeons_libraries.DungeonsLibraries;
 import com.infamous.dungeons_libraries.data.util.MergeableCodecDataManager;
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.WeightedRandom;
 import net.minecraftforge.event.AddReloadListenerEvent;
@@ -18,10 +17,10 @@ import static com.infamous.dungeons_libraries.DungeonsLibraries.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ArmoredMobConfigRegistry {
-    public static final MergeableCodecDataManager<ArmoredMobConfig, List<ArmoredMobConfig>> ARMORED_MOB_CONFIGS = new MergeableCodecDataManager<>("armored_mob", DungeonsLibraries.LOGGER, ArmoredMobConfig.CODEC, ArmoredMobConfigRegistry::armoredMobMerger);
+    public static final MergeableCodecDataManager<ArmoredMobConfigList, List<ArmoredMobConfig>> ARMORED_MOB_CONFIGS = new MergeableCodecDataManager<>("armored_mob", DungeonsLibraries.LOGGER, ArmoredMobConfigList.CODEC, ArmoredMobConfigRegistry::armoredMobMerger);
 
-    public static List<ArmoredMobConfig> armoredMobMerger(List<ArmoredMobConfig> raws){
-        return raws.stream().collect(Collectors.toList());
+    public static List<ArmoredMobConfig> armoredMobMerger(List<ArmoredMobConfigList> raws){
+        return raws.stream().flatMap(rawList -> rawList.getConfigs().stream()).collect(Collectors.toList());
     }
 
     public static ArmoredMobConfig getRandomConfig(ResourceLocation resourceLocation, Random random) {
