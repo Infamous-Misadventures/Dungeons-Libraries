562296a8bac5d4c6b847f23743a9727447738d10
Patrigan <patrick.provinciael@gmail.com>
2022-11-15 15:26:58 +0100

    Missed CuriosStopMessage

--- src/main/java/com/infamous/dungeons_libraries/integration/curios/client/message/CuriosArtifactStopMessage.java
+++ src/main/java/com/infamous/dungeons_libraries/integration/curios/client/message/CuriosArtifactStopMessage.java
@@ -6,28 +6,22 @@ import com.infamous.dungeons_libraries.items.artifacts.ArtifactItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkEvent;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 public class CuriosArtifactStopMessage {
-    private final int slot;
 
-    public CuriosArtifactStopMessage(int slot) {
-        this.slot = slot;
+    public CuriosArtifactStopMessage() {
     }
 
     public static void encode(CuriosArtifactStopMessage packet, FriendlyByteBuf buf) {
-        buf.writeInt(packet.slot);
+
     }
 
     public static CuriosArtifactStopMessage decode(FriendlyByteBuf buf) {
-        return new CuriosArtifactStopMessage(buf.readInt());
+        return new CuriosArtifactStopMessage();
     }
 
     public static class CuriosArtifactHandler {
