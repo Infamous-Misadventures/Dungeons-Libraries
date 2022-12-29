fdff296404988db9f53c1e707f81d68ab7539e4e
Patrigan <patrick.provinciael@gmail.com>
2022-12-29 12:18:14 +0100

    Dual Wield weapons only switch when holding another weapon in offhand.

--- src/main/java/com/infamous/dungeons_libraries/combat/DualWieldHandler.java
+++ src/main/java/com/infamous/dungeons_libraries/combat/DualWieldHandler.java
@@ -7,6 +7,7 @@ import com.infamous.dungeons_libraries.items.gearconfig.MeleeGearConfigRegistry;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TieredItem;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = DungeonsLibraries.MODID)
@@ -14,8 +15,9 @@ public class DualWieldHandler {
     public static void switchHand(ServerPlayer player) {
         if(!DungeonsLibrariesConfig.ENABLE_DUAL_WIELDING.get()) return;
         ItemStack mainHandItem = player.getMainHandItem();
+        ItemStack offHandItem = player.getOffhandItem();
         MeleeGearConfig config = MeleeGearConfigRegistry.getConfig(mainHandItem.getItem().getRegistryName());
-        if(config.isLight()) {
+        if(config.isLight() && !offHandItem.isEmpty() && offHandItem.getItem() instanceof TieredItem) {
             player.setItemInHand(InteractionHand.MAIN_HAND, player.getOffhandItem());
             player.setItemInHand(InteractionHand.OFF_HAND, mainHandItem);
         }
